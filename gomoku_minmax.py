cba92abd09183d1fc3bb2014f253483ef8baa7a5
kevin <kevincf.tellier@gmail.com>
2019-02-10 18:10:14 +0100

    Working MiniMax

--- gomoku_minmax.py
+++ gomoku_minmax.py
@@ -5,169 +5,184 @@ PIONS = 3
 PLAYER_1 = 1
 PLAYER_2 = 2
 EMPTY = 0
+DEPTH = 2
+WIN_WEIGHT = 1000
 
 class Node:
-    def __init__(self, child, hint, score):
-        self.childs = []
-        self.childs.append(child)
-        self.score = score
-        self.hint = hint
+	def __init__(self, child, hint, score):
+		self.childs = []
+		self.childs.append(child)
+		self.score = score
+		self.hint = hint
 
-    def add_child(self, child):
-        self.childs.append(self)
+	def add_child(self, child):
+		self.childs.append(self)
 
-    def is_terminal_node(self):
-        if self.childs is None:
-            return True
-        else:
-            return False
+	def is_terminal_node(self):
+		if self.childs is None:
+			return True
+		else:
+			return False
 
-    def to_string(self):
-        if self is is_terminal_node():
-            return self.hint[0] + ";" + self.hint[1] + " : " + self.score + "\n"
-        else:
-            string += to_string(self)
+	def to_string(self,layer=0):
+		if self is not None:
+			ret = "\t"*layer + "Hint : " + str(self.hint[0]+1) + ";" + str(self.hint[1]+1) + " | Score : " + str(self.score) + "\n"
+			for i in range(len(self.childs)):
+				if self.childs[i] is not None:
+					ret += self.childs[i].to_string(layer+1)
+			return ret
+
+	def printnodes(self,G,depth):
+		print(self)
+		if len(self.childs) is 0 or depth is 0:
+			return
+		else:
+			for child in self.childs:
+				if child is not None:
+					G.add_node(child)
+					G.add_edge(self, child)
+					child.printnodes(G,depth-1)
 
 def eval_position(P):
-    c1_max = 0
-    c2_max = 0
-    for j in range(0,N):
-        C1=0
-        C2=0
-        for i in range(0,N):#horizontal
-            if P[i][j] == PLAYER_1:
-                C2=0
-                C1+=1
-                if C1 == PIONS : return -WIN_WEIGHT
-            if P[i][j] == PLAYER_2:
-                C1=0
-                C2+=1
-                if C2 == PIONS : return WIN_WEIGHT
-            if P[i][j] == EMPTY:
-                C1=0
-                C2=0
-            if C1 > c1_max:
-                c1_max = C1
-            if C2 > c2_max:
-                c2_max = C2
+	c1_max = 0
+	c2_max = 0
+	for j in range(0,N):
+		C1=0
+		C2=0
+		for i in range(0,N):#horizontal
+			if P[i][j] == PLAYER_1:
+				C2=0
+				C1+=1
+				if C1 == PIONS : return -WIN_WEIGHT
+			if P[i][j] == PLAYER_2:
+				C1=0
+				C2+=1
+				if C2 == PIONS : return WIN_WEIGHT
+			if P[i][j] == EMPTY:
+				C1=0
+				C2=0
+			if C1 > c1_max:
+				c1_max = C1
+			if C2 > c2_max:
+				c2_max = C2
 
-    for i in range(0,N):
-        C1=0
-        C2=0
-        for j in range(0,N):#vertical
-            if P[i][j] == PLAYER_1:
-                C2=0
-                C1+=1
-                if C1 == PIONS : return -WIN_WEIGHT
-            if P[i][j] == PLAYER_2:
-                C1=0
-                C2+=1
-                if C2 == PIONS : return WIN_WEIGHT
-            if P[i][j] == EMPTY:
-                C1=0
-                C2=0
-            if C1 > c1_max:
-                c1_max = C1
-            if C2 > c2_max:
-                c2_max = C2
+	for i in range(0,N):
+		C1=0
+		C2=0
+		for j in range(0,N):#vertical
+			if P[i][j] == PLAYER_1:
+				C2=0
+				C1+=1
+				if C1 == PIONS : return -WIN_WEIGHT
+			if P[i][j] == PLAYER_2:
+				C1=0
+				C2+=1
+				if C2 == PIONS : return WIN_WEIGHT
+			if P[i][j] == EMPTY:
+				C1=0
+				C2=0
+			if C1 > c1_max:
+				c1_max = C1
+			if C2 > c2_max:
+				c2_max = C2
 
-    for rs in range(0,N-PIONS+1):
-        C1=0
-        C2=0
-        i = rs
-        j = 0
-        while i < N and j < N:
-            if P[i][j] == PLAYER_1:
-                C2=0
-                C1+=1
-                if C1 == PIONS : return -WIN_WEIGHT
-            if P[i][j] == PLAYER_2:
-                C1=0
-                C2+=1
-                if C2 == PIONS : return WIN_WEIGHT
-            if P[i][j] == EMPTY:
-                C1=0
-                C2=0
-            i+=1
-            j+=1
-            if C1 > c1_max:
-                c1_max = C1
-            if C2 > c2_max:
-                c2_max = C2
+	for rs in range(0,N-PIONS+1):
+		C1=0
+		C2=0
+		i = rs
+		j = 0
+		while i < N and j < N:
+			if P[i][j] == PLAYER_1:
+				C2=0
+				C1+=1
+				if C1 == PIONS : return -WIN_WEIGHT
+			if P[i][j] == PLAYER_2:
+				C1=0
+				C2+=1
+				if C2 == PIONS : return WIN_WEIGHT
+			if P[i][j] == EMPTY:
+				C1=0
+				C2=0
+			i+=1
+			j+=1
+			if C1 > c1_max:
+				c1_max = C1
+			if C2 > c2_max:
+				c2_max = C2
 
-    for cs in range(1,N-PIONS+1):
-        C1=0
-        C2=0
-        i = 0
-        j = cs
-        while i < N and j < N:
-            if P[i][j] == PLAYER_1:
-                C2=0
-                C1+=1
-                if C1 == PIONS : return -WIN_WEIGHT
-            if P[i][j] == PLAYER_2:
-                C1=0
-                C2+=1
-                if C2 == PIONS : return WIN_WEIGHT
-            if P[i][j] == EMPTY:
-                C1=0
-                C2=0
-            if C1 > c1_max:
-                c1_max = C1
-            if C2 > c2_max:
-                c2_max = C2
-            i+=1
-            j+=1
+	for cs in range(1,N-PIONS+1):
+		C1=0
+		C2=0
+		i = 0
+		j = cs
+		while i < N and j < N:
+			if P[i][j] == PLAYER_1:
+				C2=0
+				C1+=1
+				if C1 == PIONS : return -WIN_WEIGHT
+			if P[i][j] == PLAYER_2:
+				C1=0
+				C2+=1
+				if C2 == PIONS : return WIN_WEIGHT
+			if P[i][j] == EMPTY:
+				C1=0
+				C2=0
+			if C1 > c1_max:
+				c1_max = C1
+			if C2 > c2_max:
+				c2_max = C2
+			i+=1
+			j+=1
 
-    for rs in range(0, PIONS):
-        C1=0
-        C2=0
-        i = N-1
-        j = rs
-        while i >= 0 and j < N:
-            if P[i][j] == PLAYER_1:
-                C2=0
-                C1+=1
-                if C1 == PIONS : return -WIN_WEIGHT
-            if P[i][j] == PLAYER_2:
-                C1=0
-                C2+=1
-                if C2 == PIONS : return WIN_WEIGHT
-            if P[i][j] == EMPTY:
-                C1=0
-                C2=0
-            i-=1
-            j+=1
-            if C1 > c1_max:
-                c1_max = C1
-            if C2 > c2_max:
-                c2_max = C2
+	for rs in range(0, PIONS):
+		C1=0
+		C2=0
+		i = N-1
+		j = rs
+		while i >= 0 and j < N:
+			if P[i][j] == PLAYER_1:
+				C2=0
+				C1+=1
+				if C1 == PIONS : return -WIN_WEIGHT
+			if P[i][j] == PLAYER_2:
+				C1=0
+				C2+=1
+				if C2 == PIONS : return WIN_WEIGHT
+			if P[i][j] == EMPTY:
+				C1=0
+				C2=0
+			i-=1
+			j+=1
+			if C1 > c1_max:
+				c1_max = C1
+			if C2 > c2_max:
+				c2_max = C2
 
 
-    for cs in range(PIONS-1, N-1):
-        C1=0
-        C2=0
-        i = cs
-        j = 0
-        while i >= 0 and j < N:
-            if P[i][j] == PLAYER_1:
-                C2=0
-                C1+=1
-                if C1 == PIONS : return -WIN_WEIGHT
-            if P[i][j] == PLAYER_2:
-                C1=0
-                C2+=1
-                if C2 == PIONS : return WIN_WEIGHT
-            if P[i][j] == EMPTY:
-                C1=0
-                C2=0
-            if C1 > c1_max:
-                c1_max = C1
-            if C2 > c2_max:
-                c2_max = C2
-            i-=1
-            j+=1
-    return (c2_max - c1_max) * 100
+	for cs in range(PIONS-1, N-1):
+		C1=0
+		C2=0
+		i = cs
+		j = 0
+		while i >= 0 and j < N:
+			if P[i][j] == PLAYER_1:
+				C2=0
+				C1+=1
+				if C1 == PIONS : return -WIN_WEIGHT
+			if P[i][j] == PLAYER_2:
+				C1=0
+				C2+=1
+				if C2 == PIONS : return WIN_WEIGHT
+			if P[i][j] == EMPTY:
+				C1=0
+				C2=0
+			if C1 > c1_max:
+				c1_max = C1
+			if C2 > c2_max:
+				c2_max = C2
+			i-=1
+			j+=1
+	return (c2_max - c1_max) * 10
 
 def possible_hints(P):
 	hints = []
@@ -177,40 +192,82 @@ def possible_hints(P):
 				hints.append((x,y))
 	return hints
 
-def MinMax(node, depth, player, P):
-	if depth == 0 or node is not None or node.is_terminal_node():
-            return node.score
-	if player is PLAYER_1:
-	    value = float("-inf")
-            for child in node.childs:
-                value = max(value,MinMax(child, depth-1, PLAYER_2, P))
-	else:
-            value = float("inf")
-	    for child in node.childs:
-	        value = min(value, MinMax(child, depth-1, PLAYER_1, P))
+def play_ai(P):
+	maxi = float('-inf')
+	for hint in possible_hints(P):
+		P[hint[0]][hint[1]] = PLAYER_2
+		tmp = MinMax(DEPTH,PLAYER_2,P)
+		print(str(hint) + ":" + str(tmp))
+		if tmp > maxi:
+			maxi = tmp
+			best_hint = hint
+		P[hint[0]][hint[1]] = 0
+	P[best_hint[0]][best_hint[1]] = PLAYER_2
+	print("IA played : " + str(best_hint[0]) + ";" + str(best_hint[1]))
+
+def MinMax(depth, player, P):
+	if depth == 0:
+		return eval_position(P)
+
+	for hint in possible_hints(P):
+		if player is PLAYER_2:
+			value = float("-inf")
+			P[hint[0]][hint[1]] = PLAYER_2
+			value = max(value,MinMax(depth-1, PLAYER_2, P))
+			P[hint[0]][hint[1]] = 0
+			return value
+		else:
+			value = float("inf")
+			P[hint[0]][hint[1]] = PLAYER_1
+			value = min(value,MinMax(depth-1, PLAYER_1, P))
+			P[hint[0]][hint[1]] = 0
+			return value
 
 def construct_nodes(node,P,player,depth):
-    if depth == 0:
-    	return
-    else:
-	for hint in possible_hints(P):
-            Pp = deepcopy(P)
-	    Pp[hint[0]][hint[1]] = player#Place pion
-	    score = eval_position(P)
-	    child = Node(None, hint, score)
-	    node.add_child(child)
-        if player is PLAYER_1:
-	    construct_nodes(child,Pp,2,depth-1)
-        else:
-	    construct_nodes(child,Pp,1,depth-1)
+	if depth is 0:
+		return
+	else:
+		for hint in possible_hints(P):
+			Pp = deepcopy(P)
+			Pp[hint[0]][hint[1]] = player#Place pion
+			score = eval_position(Pp)
+			child = Node(None, hint, score)
+			node.add_child(child)
+			if player is PLAYER_1:
+				construct_nodes(child,Pp,PLAYER_2,depth-1)
+			else:
+				construct_nodes(child,Pp,PLAYER_1,depth-1)
+
+def print_board(P):
+	print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in P]))
+
+def play_player(P):
+	print("It's your turn you fool")
+	hint_x = int(input("x: "))
+	hint_y = int(input("y: "))
+	while P[hint_y][hint_x] != 0:
+		print("Impossible")
+		hint_x = int(input("x: "))
+		hint_y = int(input("y: "))
+	P[hint_y][hint_x] = PLAYER_1
+	print("You played : " + str(hint_x) + ";" + str(hint_y))
 
 def main():
-    P = [[0]*N for _ in range(N)] 
-    node = Node(None,(0,0),0)
-    print("---Nodes Started---\n")
-    construct_nodes(node,P,2,4)
-    print("---Nodes OK---\n");
-    hint = MinMax(node,3,1,P)
-    print(hint)
+	P = [[0]*N for _ in range(N)]
+	while 1:
+		print_board(P)
+		print(eval_position(P))
+		play_player(P)
+		print_board(P)
+		print(eval_position(P))
+		if eval_position(P) == -WIN_WEIGHT:
+			print("Player 1 won !")
+			break
+		play_ai(P)
+		if eval_position(P) == WIN_WEIGHT:
+			print_board(P)
+			print("Player 2 won !")
+			break
 main()
-    
+	
--- tree.py
+++ tree.py
@@ -1,84 +1,217 @@
-import numpy as np
-PIONS = 2
+from modules.queue import Queue
+from copy import deepcopy as deepcopy
+import sys
 
-class Tree:
-    def __init__(self, branch, hint, score):
-        self.branch = []
-        self.branch.append(branch)
-        if branch is not None:
-            self.score = score + branch.score
+class Node:
+  def __init__(self, data = None):
+    self.data = data
+    self.left = None
+    self.right = None
+
+  def visit(self):
+    sys.stdout.write(self.data)
+
+  def getNumNodes(self):
+    total = 0
+    if self.left:
+      total += self.left.getNumNodes()
+    if self.right:
+      total += self.right.getNumNodes()
+    return total + 1
+
+
+  @classmethod
+  def createTree(cls, depth):
+    tree = Node('X')
+    cls.createTreeHelper(tree, depth, 1)
+    return tree
+
+  @classmethod
+  def createTreeHelper(cls, node, depth, cur):
+    if cur == depth:
+      return
+    node.left = Node('X')
+    node.right = Node('XX')
+    cls.createTreeHelper(node.left, depth, cur + 1)
+    cls.createTreeHelper(node.right, depth, cur + 1)
+
+
+  def getHeight(self):
+    return Node.getHeightHelper(self)
+
+  @staticmethod
+  def getHeightHelper(node):
+    if not node:
+      return 0
+    else:
+      return max(Node.getHeightHelper(node.left), Node.getHeightHelper(node.right)) + 1
+
+  def fillTree(self, height):
+    Node.fillTreeHelper(self, height)
+
+  def fillTreeHelper(node, height):
+    if height <= 1:
+      return
+    if node:
+      if not node.left: node.left = Node(' ')
+      if not node.right: node.right = Node(' ')
+      Node.fillTreeHelper(node.left, height - 1)
+      Node.fillTreeHelper(node.right, height - 1)
+
+
+  def prettyPrint(self):
+    """
+    """
+    # get height of tree
+    total_layers = self.getHeight()
+
+    tree = deepcopy(self)
+
+    tree.fillTree(total_layers)
+    # start a queue for BFS
+    queue = Queue()
+    # add root to queue
+    queue.enqueue(tree) # self = root
+    # index for 'generation' or 'layer' of tree
+    gen = 1 
+    # BFS main
+    while not queue.isEmpty():
+      # copy queue
+      # 
+      copy = Queue()
+      while not queue.isEmpty():
+        copy.enqueue(queue.dequeue())
+      # 
+      # end copy queue 
+
+      first_item_in_layer = True
+      edges_string = ""
+      extra_spaces_next_node = False
+
+      # modified BFS, layer by layer (gen by gen)
+      while not copy.isEmpty():
+
+        node = copy.dequeue()
+
+        # -----------------------------
+        # init spacing
+        spaces_front = pow(2, total_layers - gen + 1) - 2
+        spaces_mid   = pow(2, total_layers - gen + 2) - 2
+        dash_count   = pow(2, total_layers - gen) - 2
+        if dash_count < 0:
+          dash_count = 0
+        spaces_mid = spaces_mid - (dash_count*2)
+        spaces_front = spaces_front - dash_count
+        init_padding = 2
+        spaces_front += init_padding
+        if first_item_in_layer:
+          edges_string += " " * init_padding
+        # ----------------------------->
+
+        # -----------------------------
+        # construct edges layer
+        edge_sym = "/" if node.left and node.left.data is not " " else " "
+        if first_item_in_layer:
+          edges_string += " " * (pow(2, total_layers - gen) - 1) + edge_sym
         else:
-            self.score = score
-        self.hint = hint
+          edges_string += " " * (pow(2, total_layers - gen + 1) + 1) + edge_sym
+        edge_sym = "\\" if node.right and node.right.data is not " " else " "
+        edges_string += " " * (pow(2, total_layers - gen + 1) - 3) + edge_sym
+        # ----------------------------->
 
-    def get_next_branch(self, pos):
-        for branch in self.branch:
-            if branch != None:
-                if pos == branch.hint:
-                    return branch
-        return -1
+        # -----------------------------
+        # conditions for dashes
+        if node.left and node.left.data == " ":
+          dash_left = " "
+        else:
+          dash_left = "_"
 
-    def add_branch(self, tree):
-        self.branch.append(tree)
-        self.score += tree.score
-    
-    def get_best_branch(self):
-        score_max = -600*PIONS
-        for branch in self.branch:
-            if branch != None:
-                if branch.score > score_max:
-                    best_branch = branch
-                    score_max = branch.score
-        return best_branch
+        if node.right and node.right.data == " ":
+          dash_right = " "
+        else:
+          dash_right = "_"
+        # ----------------------------->
 
-    def to_string(self, layer=0):
-        if self is not None:
-            ret = "\t"*layer + "Hint : " + str(self.hint) + " | Score : " + str(self.score) + "\n"
-            for i in range(len(self.branch)):
-                if self.branch[i] is not None:
-                    ret += self.branch[i].to_string(layer+1)
-            return ret
+        # -----------------------------
+        # handle condition for extra spaces when node lengths don't match or are even:
+        if extra_spaces_next_node:
+          extra_spaces = 1
+          extra_spaces_next_node = False
+        else:
+          extra_spaces = 0
+        # ----------------------------->
+        
+        # -----------------------------
+        # account for longer data
+        data_length = len(str(node.data))
+        if data_length > 1:
+          if data_length % 2 == 1: # odd
+            if dash_count > 0:
+              dash_count -= ((data_length - 1)/2)
+            else:
+              spaces_mid -= (data_length - 1)/2
+              spaces_front -= (data_length - 1)/2
+              if data_length is not 1: 
+                extra_spaces_next_node = True 
+          else: # even
+            if dash_count > 0:
+              dash_count -= ((data_length)/2) - 1
+              extra_spaces_next_node = True
+              # dash_count += 1
+            else:
+              spaces_mid -= (data_length - 1)
+              spaces_front -= (data_length - 1)
+        # ----------------------------->
+        
+        # -----------------------------
+        # print node with/without dashes
+        if first_item_in_layer:
+          print (" " * spaces_front) + (dash_left * dash_count) + (node.data) + (dash_right * dash_count),
+          first_item_in_layer = False
+        else:
+          print (" " * (spaces_mid-extra_spaces)) + (dash_left * dash_count) + (node.data) + (dash_right * dash_count),
+        # ----------------------------->
+
+        if node.left: queue.enqueue(node.left)
+        if node.right: queue.enqueue(node.right)
+
+      # print the fun squiggly lines
+      if not queue.isEmpty():
+        print "\n" + edges_string
+
+      # increase layer index
+      gen += 1
+      
 
 
-T0 = Tree(None,(5,5),0)
+if __name__ == '__main__':
+  # prep the tree...
+  # 
+  # layer 1
+  root = Node('A')
 
-T1 = Tree(None,(5,4),-100)
-T2 = Tree(None,(5,3),-100)
-T3 = Tree(None,(5,2),-100)
-T4 = Tree(None,(5,1),0)
+  # layer 2
+  root.left = Node('B')
+  root.right = Node('C')
 
-T5 = Tree(None,(4,5),100)
+  # layer 3
+  root.left.left = Node('D')
+  root.left.right = Node('E')
 
-T6 = Tree(None,(4,4),100)
+  root.left.right.right = Node.createTree(2)
 
-T7 = Tree(None,(4,3),1000)
+  root.right.left = Node('F')
+  root.right.right = Node('G')
 
-T6.add_branch(T7)
+  # layer 3
+  root.left.left.left = Node('H')
+  root.left.left.right = Node('I')
+  root.left.right.left = Node('J')
+  # root.left.right.right = Node('K')
+  # root.right.left.left = Node('L')
+  # root.right.left.right = Node('M')
+  root.right.right.left = Node('N')
+  root.right.right.right = Node('O')
 
-T4.add_branch(T6)
-T4.add_branch(T5)
-
-T0.add_branch(T1)
-T0.add_branch(T2)
-T0.add_branch(T3)
-T0.add_branch(T4)
-
-print(T0.to_string())
-best_branch = T0
-print("P1 : " + str(best_branch.hint))
-
-#P2 played 5,5
-print("P1 : (5, 5)")
-
-best_branch = T0.get_best_branch()
-print("P2 : " + str(best_branch.hint))
-#P1 played 5,1
-
-best_branch = best_branch.get_next_branch((4,4))
-print("P1 : " + str(best_branch.hint))
-#P2 played 4,4
-
-best_branch = best_branch.get_best_branch()
-print("P2 : " + str(best_branch.hint))
-#P1 played 4,3
+  root.prettyPrint()
 
